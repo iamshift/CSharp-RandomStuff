6283be32fffe9553bb06227441ac448853d43e90
Jorge Pires <deathitterpt@gmail.com>
2021-08-05 12:47:42 +0100

    Update FastParse.cs

--- FastParse.cs
+++ FastParse.cs
@@ -1,32 +1,31 @@
 using System.Linq;
 
-namespace SecurityPlugin
+
+public class FastParse
 {
-    public class FastParse
+    public static short ShortParse(string value)
     {
-        public static short ShortParse(string value)
-        {
-            return value.Aggregate<char, short>(0, (current, character) => (short) (10 * current + (character - 48)));
-        }
-        
-        public static int IntParse(string value)
-        {
-            return value.Aggregate<char, int>(0, (current, character) => 10 * current + (character - 48));
-        }
+        return value.Aggregate<char, short>(0, (current, character) => (short) (10 * current + (character - 48)));
+    }
 
-        public static long LongParse(string value)
-        {
-            return value.Aggregate<char, long>(0, (current, character) => 10 * current + (character - 48));
-        }
+    public static int IntParse(string value)
+    {
+        return value.Aggregate<char, int>(0, (current, character) => 10 * current + (character - 48));
+    }
 
-        public static double DoubleParse(string value)
-        {
-            return value.Aggregate<char, double>(0, (current, character) => 10 * current + (character - 48));
-        }
+    public static long LongParse(string value)
+    {
+        return value.Aggregate<char, long>(0, (current, character) => 10 * current + (character - 48));
+    }
 
-        public static float FloatParse(string value)
-        {
-            return value.Aggregate<char, float>(0, (current, character) => 10 * current + (character - 48));
-        }
+    public static double DoubleParse(string value)
+    {
+        return value.Aggregate<char, double>(0, (current, character) => 10 * current + (character - 48));
+    }
+
+    public static float FloatParse(string value)
+    {
+        return value.Aggregate<char, float>(0, (current, character) => 10 * current + (character - 48));
     }
 }
+
