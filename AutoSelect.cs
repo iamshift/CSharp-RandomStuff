f802d8a104286782cba25bc92361d4b9854cda4c
Jorge Pires <deathitterpt@gmail.com>
2021-08-05 12:47:27 +0100

    Update AutoSelect.cs

--- AutoSelect.cs
+++ AutoSelect.cs
@@ -3,31 +3,29 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace primemakers.runner.menus
+public class AutoSelect : MonoBehaviour
 {
-    public class AutoSelect : MonoBehaviour
-    {
-        [SerializeField] private bool _isPopupButton;
-        private Selectable _selected;
-        
-        private void OnEnable()
-        {
-            if(_isPopupButton && EventSystem.current != null && EventSystem.current.currentSelectedGameObject != null)
-                _selected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
-            
-            StartCoroutine(SelectButton(GetComponent<Selectable>()));
-        }
+    [SerializeField] private bool _isPopupButton;
+    private Selectable _selected;
 
-        private void OnDisable()
-        {
-            if(_isPopupButton)
-                _selected?.Select();
-        }
-        
-        private IEnumerator SelectButton(Selectable button)
-        {
-            yield return new WaitForSeconds(.02F);
-            button?.Select();
-        }
+    private void OnEnable()
+    {
+        if(_isPopupButton && EventSystem.current != null && EventSystem.current.currentSelectedGameObject != null)
+            _selected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+
+        StartCoroutine(SelectButton(GetComponent<Selectable>()));
+    }
+
+    private void OnDisable()
+    {
+        if(_isPopupButton)
+            _selected?.Select();
+    }
+
+    private IEnumerator SelectButton(Selectable button)
+    {
+        yield return new WaitForSeconds(.02F);
+        button?.Select();
     }
 }
+
