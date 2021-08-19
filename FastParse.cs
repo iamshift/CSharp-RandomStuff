b3f4c6c4b04bccf1e570da43e369d22adb370799
Jorge Pires <jorge_pires_12@hotmail.com>
2021-08-19 17:56:12 +0100

    Update FastParse.cs

--- FastParse.cs
+++ FastParse.cs
@@ -1,31 +1,32 @@
 using System.Linq;
 
-
-public class FastParse
+namespace JoPires
 {
-    public static short ShortParse(string value)
+    public class FastParse
     {
-        return value.Aggregate<char, short>(0, (current, character) => (short) (10 * current + (character - 48)));
-    }
+        public static short ShortParse(string value)
+        {
+            return value.Aggregate<char, short>(0, (current, character) => (short) (10 * current + (character - 48)));
+        }
 
-    public static int IntParse(string value)
-    {
-        return value.Aggregate<char, int>(0, (current, character) => 10 * current + (character - 48));
-    }
+        public static int IntParse(string value)
+        {
+            return value.Aggregate<char, int>(0, (current, character) => 10 * current + (character - 48));
+        }
 
-    public static long LongParse(string value)
-    {
-        return value.Aggregate<char, long>(0, (current, character) => 10 * current + (character - 48));
-    }
+        public static long LongParse(string value)
+        {
+            return value.Aggregate<char, long>(0, (current, character) => 10 * current + (character - 48));
+        }
 
-    public static double DoubleParse(string value)
-    {
-        return value.Aggregate<char, double>(0, (current, character) => 10 * current + (character - 48));
-    }
+        public static double DoubleParse(string value)
+        {
+            return value.Aggregate<char, double>(0, (current, character) => 10 * current + (character - 48));
+        }
 
-    public static float FloatParse(string value)
-    {
-        return value.Aggregate<char, float>(0, (current, character) => 10 * current + (character - 48));
+        public static float FloatParse(string value)
+        {
+            return value.Aggregate<char, float>(0, (current, character) => 10 * current + (character - 48));
+        }
     }
 }
-
